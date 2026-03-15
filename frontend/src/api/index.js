a3947334f9e02d1ddfac663f7ef386f73b736beb
Sisyetad <sisaytadewos@gmail.com>
2026-03-15 21:00:58 +0300

    editting some issues and endpoint for deployin

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://health-data-analytics-7hm2.onrender.com",
   timeout: 30000,
 });
 
